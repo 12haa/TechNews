960de053212b850d22700d23458d7e65806208fd
Mohamad Khaefi <mkhaefi2500@yahoo.com>
2024-06-04 11:57:14 +0330

    Changed [...nextauth]/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -21,5 +21,7 @@ export const authOptions: AuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+
+export default function handler(req, res) {
+  return NextAuth(authOptions)(req, res);
+}
